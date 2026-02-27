96704cd7f40e7753e9e4eefa0275f630c13b1ace
Lakatos Tamás <tamas.lakatos1@student.uni-miskolc.hu>
2026-02-27 14:32:33 +0100

    modify

---
+++ src/main/java/com/example/demo/Manufacturer_Services.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public interface Manufacturer_Services {
+    void addManufacturer(Manufacturer manufacturer);
+    Manufacturer getManufacturerByName(String name);
+    void updateManufacturer(Manufacturer manufacturer);
+    void deleteManufacturer(String name);
+}
